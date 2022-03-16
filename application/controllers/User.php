7968aaf83cc6b99f79130df8810175c8a04ccd7b
projectandre <andreagung777@gmail.com>
2022-03-16 13:32:52 +0700

    Tugas1

---
+++ application/controllers/User.php
@@ -0,0 +1,8 @@
+<?php
+class User extends CI_Controller
+{
+    public function add()
+    {
+        $this->load->view('user_add');
+    }
+}
---
+++ application/views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website me</title>
+</head>
+
+<body>
+
+    <h1>Hello saya andre agung!!</h1>
+    <h1>Npm saya 2007051035 kak!</h1>
+    <h1>Makasih..</h1>
+
+</body>
+
+</html>
